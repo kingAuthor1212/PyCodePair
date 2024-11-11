3b83e8565a5a5f4233ee63865cbcfd904480fe1b
namotswjhb024 <namotswjhb024@student.wethinkcode.co.za>
2024-11-11 21:55:33 +0200

    Modified code

--- bootcamp_exercise_marker/bootcamp_exercise_marker.py
+++ bootcamp_exercise_marker/bootcamp_exercise_marker.py
@@ -1,11 +1,17 @@
 # Abdur-Raheem Lee <abdur-raheem@wethinkcode.co.za>
+import random
 
 def read_file():
     '''
     Reads contents from the text file (questions.txt)
     @return a list of five random questions
     '''
-
+    #Naledi made changes:
+    with open ("questions.txt", "r") as questions:
+        questions_list = questions.readlines()
+        random.shuffle(questions_list)
+        list_of_questions = question_list[:5]
+    return list_of_questions
 
 def ask_questions(list_of_questions):
     '''
@@ -13,10 +19,13 @@ def ask_questions(list_of_questions):
     @param list of five questions
     @return a list of questions the user answer incorrectly
     '''
+    if not list_of_questions or not isinstance(list_of_questions, list):
+        raise ValueError("list_of_questions must be a non-empty list")
+    
     incorrect_question = []
     for i, question in enumerate(list_of_questions):
         userAnswer = display_question(i + 1, question)
-        correct_answer = question.split(', ')[1]
+        correct_answer = question.split(', ')[1].strip()
         if not is_correct_answer(correct_answer, userAnswer):
             incorrect_question.append(question)
     return incorrect_question
@@ -29,9 +38,9 @@ def display_question(question_number, question):
     @param a single question
     @return the answer given by the user
     '''
-    quest_parts = question.split(', ')
-    print(f"{question_number}.{quest_parts[0]}")
-    for option in quest_parts[2:]:
+    q_list = question.split(', ')
+    print(f"{question_number}. {q_list[0]}")
+    for option in q_list[1:]:
         print(option)
     user_answer = input("Your answer (A, B, C :) ").strip.upper()
     return user_answer
